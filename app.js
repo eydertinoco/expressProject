372d0db015ab3d3c2ae0b4e9c682a7fea94c6aad
Eyder <etinoco@indracompany.com>
2022-10-14 08:31:17 -0300

    - Reiniciando o Projeto

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-require('dotenv').config();
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,47 +16,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const { allowedNodeEnvironmentFlags } = require('process');
+// Rotas
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Rotas da API
-const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
-const booksRouter = require('./routes/books');
-app.use('/books', booksRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+// Static Files
 
-const PORT = process.env.PORT | 3000;
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.bf6kvlp.mongodb.net/?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log("Conectamos ao MongoDB");
-        app.listen(PORT, '0.0.0.0', () => {
-            console.log(`Servidor escurando em http://localhost:${PORT}`)
-        })
-    })
-    .catch((err) => console.log(err))
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// app.on('open', () => {
-//   app.listen(port, () => {
-//     console.log(`Servidor escurando em http://localhost:${port}`)
-//   })
-// })
-//
-// module.exports = app;
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
